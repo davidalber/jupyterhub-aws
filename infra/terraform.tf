f686e80ee3079ce335b9b221b54c7ac52651ac8c
David Alber <alber.david@gmail.com>
2024-03-07 12:32:42 -0800

    Configure S3 backend

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = "~> 1.7.0"
 
+  backend "s3" {
+    bucket = "dma-tf-state"
+    key    = "jupyterhub-aws/terraform.tfstate"
+    region = "us-west-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
