813303f584affc52679c882cfd0559c3e08be6cc
David Alber <alber.david@gmail.com>
2024-03-07 13:02:09 -0800

    Add descriptions to variables

--- infra/vars.tf
+++ infra/vars.tf
@@ -1,11 +1,13 @@
 variable "aws_region" {
-  type     = string
-  nullable = false
-  default  = "us-west-2"
+  type        = string
+  nullable    = false
+  default     = "us-west-2"
+  description = "AWS region in which resources are create."
 }
 
 variable "vpc_name" {
-  type     = string
-  nullable = false
-  default  = "jupyterhub"
+  type        = string
+  nullable    = false
+  default     = "jupyterhub"
+  description = "Name of the VPC."
 }
